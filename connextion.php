da190809f469e94cddddcd2eadfa649cbee51318
Joel Tabasco <62035897+jtabasco@users.noreply.github.com>
2025-01-21 12:36:58 -0700

    Actualizar connextion.php

--- connextion.php
+++ connextion.php
@@ -2,8 +2,8 @@
 // Conexión a la base de datos
 $servername = "jtabasco.com";
 $username = "u338215117_joelgasto";
-//$password = "5Yv=3f/@m";
-$password = "c4C~=ns+L=";
+$password = "5Yv=3f/@m";
+//$password = "c4C~=*@$ns+L=";
 $dbname = "u338215117_gastos";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
