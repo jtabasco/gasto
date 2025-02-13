052c85d14016591f4eab1c874f528a32104b94e0
Joel Tabasco <62035897+jtabasco@users.noreply.github.com>
2025-02-13 16:50:00 -0700

    Update mail_config.php

--- config/mail_config.php
+++ config/mail_config.php
@@ -1,14 +1,10 @@
 <?php
 return [
-    'host' => 'smtp.hostinger.com',
+    'host' => '',
     'username' => 'administrador@jtabasco.com',
-    'password' => 'HUAOn$wX&3',
+    'password' => '',
     'port' => 587,
     'from' => 'administrador@jtabasco.com'
 
-    //'host' => 'smtp.gmail.com',
-    //'username' => 'jtabascoh@gmail.com',
-    //'password' => 'Jtlldmo2006*@',
-    //'port' => 587,
-    //'from' => 'jtabascoh@gmail.com'
+
 ]; 
